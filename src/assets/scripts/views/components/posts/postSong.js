073022f87ee9cdd108c81fe2f2a0d90f2891c7bf
Andres Auais <andresauais@gmail.com>
2021-01-19 17:53:47 +0000

    css and sections

--- src/assets/scripts/views/components/posts/postSong.js
+++ src/assets/scripts/views/components/posts/postSong.js
@@ -11,19 +11,28 @@ export const postSong = ({
   trackViewUrl,
   previewUrl,
 }) => {
+  const date = releaseDate.split("T")[0];
+  const durationMin = (trackTimeMillis/60000).toFixed(0);
+  const durationSec = ((trackTimeMillis%60000)/1000).toFixed(0);
   const template = `
     <section class="post-item">
-      <section class="post-item__img" style="background-image: url(${artworkUrl100})"></section>
-      <p class="post-item__name">name: ${trackName}</p>
-      <p class="post-item__artist">artist: ${artistName}</p>
-      <p class="post-item__album">album: ${collectionName}</p>
-      <p class="post-item__price">price: ${trackPrice} ${currency}</p>
-      <p class="post-item__release">Release date: ${releaseDate}</p>
-      <p class="post-item__length">Song length: ${trackTimeMillis}</p>
-      <p class="post-item__genre">Genre: ${primaryGenreName}</p>
-      <a class="post-item__link" href=${trackViewUrl}>Itunes link</a>
-      <button class="post-item__play" data-preview=${previewUrl}></button>
-      <button class="post-item__fav"></button>
+      <section class="post-item__img col-3" style="background-image: url(${artworkUrl100})"></section>
+      <section class="col-6">
+        <p class="post-item__name">Name: ${trackName}</p>
+        <p class="post-item__artist">Artist: ${artistName}</p>
+        <p class="post-item__album">Album: ${collectionName}</p>
+        <p class="post-item__genre">Genre: ${primaryGenreName}</p>
+        <p class="post-item__length">Duration: ${durationMin}:${durationSec}</p>
+        <p class="post-item__release">Release: ${date}</p>
+      </section>
+      <section class="col-3">
+        <p class="post-item__price">${trackPrice} ${currency}</p>
+        <section class="post-item__icons">
+          <a class="post-item__link col-6" href=${trackViewUrl}><i class="bx bxs-music"></i></a>
+          <button class="post-item__play bx bx-play col-6" data-preview=${previewUrl}></button>
+        </section>
+      </section>
+      <!--<button class="post-item__fav"></button>-->
     </section>`;
   return template;
 };
