70edc15a5e94df9fc2d6a1f1498d23650a60391f
Raúl Cátedra <raulcabe3003@gmail.com>
2021-01-20 20:31:34 +0100

    fixed radiobutons

--- src/assets/scripts/views/components/favourite.js
+++ src/assets/scripts/views/components/favourite.js
@@ -1,13 +1,13 @@
 export const favourite = () => {
   const template = `
   <section class="favourite-radio">
-    <input type="radio" id="favSong" name="searchOrfav" data-type="song" class="favourite-radio__page">
+    <input type="radio" id="favSong" name="searchOrfavPage" data-type="song" class="favourite-radio__page">
     <label for="favSong">Songs</label>
-    <input type="radio" id="favAlbum" name="searchOrfav" data-type="album" class="favourite-radio__page">
+    <input type="radio" id="favAlbum" name="searchOrfavPage" data-type="album" class="favourite-radio__page">
     <label for="favAlbum">Albums</label>
-    <input type="radio" id="favArtist" name="searchOrfav" data-type="artist" class="favourite-radio__page">
+    <input type="radio" id="favArtist" name="searchOrfavPage" data-type="artist" class="favourite-radio__page">
     <label for="favArtist">Artists</label>
-    <input type="radio" id="favVideo" name="searchOrfav" data-type="video" class="favourite-radio__page">
+    <input type="radio" id="favVideo" name="searchOrfavPage" data-type="video" class="favourite-radio__page">
     <label for="favVideo">Videos</label>
   </section>
   <section id="favRoot" class="fav-root"></section>
